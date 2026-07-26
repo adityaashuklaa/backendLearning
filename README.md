bfa1cc8a561f7e6fb8cde5c6f705e5fd5d0b0966
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-07-27 02:09:07 +0530

    created server for the lecture2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - Initializing empty node.js project, using "npm init" command.
 - React Works on a concept of hot reloading, so whenever a change is made you don't have to restart the server, it's been restarted by itself, but in express, you have to kill the terminal and restart it again, whenever changes are made.
 - To solve this issue, Nodemon is installed.
+- import express from "express" => this is a module type import, which we have to defined in the package.json file.
---
+++ lecture2/backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Server is ready")
+})
+
+// get a list of 5 jokes
+
+app.get("/jokes", (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A Joke',
+            content: 'This is a joke.'
+        },
+        {
+            id: 2,
+            title: 'Another Joke',
+            content: 'This is an another joke.'
+        },
+        {
+            id: 3,
+            title: 'A Third Joke',
+            content: 'This is a third joke.'
+        },
+        {
+            id: 4,
+            title: 'This is a Fourth Joke',
+            content: 'This is a fourth joke.'
+        },
+    ]
+    res.send(jokes)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving at port ${port}`); 
+})

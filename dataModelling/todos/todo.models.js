e76ec7bb04b3c1d42834f6a81bca0f4d2a2e4869
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-07-29 01:53:40 +0530

    Created Schema and Relation for Todos, Subtodos and User

--- dataModelling/todos/todo.models.js
+++ dataModelling/todos/todo.models.js
@@ -2,8 +2,27 @@ import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema(
     {
-
+        content: {
+            type: String,
+            required: true,
+        },
+        complete: {
+            type: Boolean,
+            default: false // Can be added with any field, but here it makes sense.
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User" // The name inside mongoose.model is being called here.
+        },
+        subTodos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "SubTodo"
+            }
+        ] // Array of subTodos
     }, {timestamps: true}
 )
 
 export const Todo = mongoose.model("Todo", todoSchema)
+
+// createdBy is a special type which requries a reference to create a relation with another schema.

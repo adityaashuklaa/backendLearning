31db2b9e3ed0aa092bae7e9d10168efec99ffea9
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-08-08 01:43:27 +0530

    Created User Model Schema

---
+++ apiError/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose, {Schema} from "mongoose"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // Cloudinary URL
+            required: true,
+        }, 
+        coverImage: {
+            type: String
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {timestamps: true}
+)
+
+export const User = mongoose.model("User", userSchema)
+
+
+// Whenever mongoDB saves user it generates a random ID in the bson data format. 
+// In any DB if you want to make a model searchable and increase it's optimization then index is being used.

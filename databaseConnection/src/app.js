8f0c86e260b2d9cce565f836aae16a2c91855d01
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-08-22 06:48:05 +0530

    Introduced cookie-parser and cors

--- databaseConnection/src/app.js
+++ databaseConnection/src/app.js
@@ -1,5 +1,21 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // Data is coming from URL.
+app.use(express.static("public"))
+app.use(cookieParser()) // Cookie parser helps the server to communicate with user's browser to get the access of cookies and set cookies, basically performing CRUD Operations. Somewhere secured cookies are being stored at browser level, which only the server can read and access.
+
 export default app
+
+// app.use is used for middlewares and configuration.
+// Earlier express don't used to accept json files, that's why body parser was used, but now with the updated version of express you don't need to add body parser, it is present by default.
+// Multer is used for file uploading.

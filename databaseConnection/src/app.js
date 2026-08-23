027bc41eff74936aca45457d26be1d392af42676
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-08-23 21:40:58 +0530

    introduced express.static

--- databaseConnection/src/app.js
+++ databaseConnection/src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // Data is coming from URL.
-app.use(express.static("public"))
+app.use(express.static("public")) // Used to store public assets, like images, videos, pdfs etc.
 app.use(cookieParser()) // Cookie parser helps the server to communicate with user's browser to get the access of cookies and set cookies, basically performing CRUD Operations. Somewhere secured cookies are being stored at browser level, which only the server can read and access.
 
 export default app

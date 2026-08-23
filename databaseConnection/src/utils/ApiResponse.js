2fc51be62a47c2ad4720d71baec8ade9cf48353b
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-08-23 23:37:52 +0530

    HTTP Response Status codes

--- databaseConnection/src/utils/ApiResponse.js
+++ databaseConnection/src/utils/ApiResponse.js
@@ -6,3 +6,12 @@ class ApiResponse {
         this.success = statusCode < 400
     }
 }
+
+// HTTP Response Status Codes
+/* 
+    Informational responses (100-199)
+    Successful responses (200-299)
+    Redirection messages (300-399)
+    CLient error responses (400-499)
+    Server error responses (500-599)
+*/

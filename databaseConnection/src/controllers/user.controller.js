55b66aa9da2e93973517ebcb5e7a0214a88244c2
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-08-27 00:46:50 +0530

    Steps to register user

--- databaseConnection/src/controllers/user.controller.js
+++ databaseConnection/src/controllers/user.controller.js
@@ -2,6 +2,14 @@ import asyncHandler from "../utils/asyncHandler"
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend (No need to write frontend, we can simulate it using postman.)
+    // validation of user details - not empty
+    // check if user already exits - unique email || unique username
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in DB.
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
 })
 
 export default registerUser

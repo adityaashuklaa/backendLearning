f73b99d1349ebeab57203f5c73b02863e7e8ee47
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-08-27 00:43:35 +0530

    Starting with registering user

--- databaseConnection/src/controllers/user.controller.js
+++ databaseConnection/src/controllers/user.controller.js
@@ -1,9 +1,7 @@
 import asyncHandler from "../utils/asyncHandler"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // get user details from frontend (No need to write frontend, we can simulate it using postman.)
 })
 
 export default registerUser

99f142f41b1167d2670c02fcab8948b8fd165853
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-09-12 06:45:07 +0530

    Updated Status Codes

--- databaseConnection/src/controllers/user.controller.js
+++ databaseConnection/src/controllers/user.controller.js
@@ -60,7 +60,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(500, "Something went wrong while registering the user!!")
     }
 
-    return res.status(202).json(
+    return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered Successfully!!")
     )
 

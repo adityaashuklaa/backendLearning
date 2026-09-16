861cf3ed499aeed3ca8d9b242edf435c0e338ed5
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-09-16 08:06:23 +0530

    Refresh and Access Tokens

--- databaseConnection/src/controllers/user.controller.js
+++ databaseConnection/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from "../utils/ApiError"
 import {User} from "../models/user.model.js"
 import uploadOnCloudinary from "../utils/cloudinary.js"
 import ApiResponse from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshTokens = async(userId) => {
     {
@@ -98,9 +99,13 @@ const loginUser = asyncHandler( async (req, res) => {
     // send cookie (secure cookie)
 
     const {email, username, password} = req.body
-    if(!username || !email) {
+    if(!username && !email) {
         throw new ApiError(400, "username or email is required.")
     }
+    // Alternative of above code.
+    // if(!(username || email)) {
+    //     throw new ApiError(400, "username or email is required.")
+    // }
 
     const user = await User.findOne({
         $or: [{username}, {email}]
@@ -160,7 +165,53 @@ const logoutUser = asyncHandler( async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
-export default { registerUser, loginUser, logoutUser }
+const refreshAccessToken = asyncHandler( async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if(!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+
+        const user = await User.findById(decodedToken?._id)
+
+        if(!user) {
+            throw new ApiError(401, "Invalid Refresh Token")
+        }
+
+        if(incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is esxpired or used.")
+        }
+        
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user_.id)
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken: newRefreshToken},
+                "Access Token Refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Refresh Token")
+    }
+})
+
+export default { registerUser, loginUser, logoutUser, refreshAccessToken }
 
 // Because the cloudinary is an expensive function and it has to awaited we have used async in the parameter of the function.
 // mongoose generates bson data, and here in registerUserData, the id is bson_id.

96acfbfd4c2233ede7c65a6cc145a10656cc1fd6
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-09-18 07:26:54 +0530

    controller for changing current user old password

--- databaseConnection/src/controllers/user.controller.js
+++ databaseConnection/src/controllers/user.controller.js
@@ -211,6 +211,22 @@ const refreshAccessToken = asyncHandler( async (req, res) => {
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?.id)
+    const isPasswordCorrect = user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid Old Password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully."))
+})
+
 export default { registerUser, loginUser, logoutUser, refreshAccessToken }
 
 // Because the cloudinary is an expensive function and it has to awaited we have used async in the parameter of the function.
--- databaseConnection/src/models/subscription.model.js
+++ databaseConnection/src/models/subscription.model.js
@@ -4,7 +4,11 @@ const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User"
-    }
+    },
+    subscriber: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing.
+        ref: "User"
+    },
 }, {timestamps: true})
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)

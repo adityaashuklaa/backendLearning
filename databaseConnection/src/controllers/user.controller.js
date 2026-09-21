29da2f72506096a6956e83e34fe3f4dd98a0c526
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-09-22 00:54:34 +0530

    Updated User Controller

--- databaseConnection/src/controllers/user.controller.js
+++ databaseConnection/src/controllers/user.controller.js
@@ -146,8 +146,11 @@ const loginUser = asyncHandler( async (req, res) => {
 const logoutUser = asyncHandler( async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id, {
-            $set: {
-                refreshToken: undefined
+            // $set: {
+            //     refreshToken: undefined
+            // }, (Better Approch)
+            $unset: {
+                refreshToken: 1
             }
         },
         {
